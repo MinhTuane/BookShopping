014e3fd0e459fe009c1123d6d5ee989a55e97ee5
Minh Tuan <148312088+MinhTuane@users.noreply.github.com>
2024-06-12 22:43:38 +0700

    Add project files.

---
+++ BookShopping/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShopping.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        [Required]
+        public int GenId { get; set; }
+        public Genre Genre;
+        public List<OrderDetail> OrderDetails { get; set; }
+        public List<CartDetail> CartDetails { get; set; }
+    }
+}
---
+++ BookShopping/Models/ShoppingCart.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShopping.Models
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
